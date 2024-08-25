1575a55fb1a4ffa71bfeff40fcd7949a79c5ce4a
pestrukha <sturbauen@gmail.com>
2024-08-25 18:56:09 +0400

    add errorhandler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -23,6 +23,16 @@ app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 init_db_pool(app.config['DATABASE_URL'])
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
